b60fa2a9fdc7dad396e7b717f92281bda304a598
LaelRodrigues <laelrodrigues7@gmail.com>
2017-03-12 15:13:03 -0300

    Implementação dos arquivos perimetro.h e perimetro.cpp

---
+++ src/perimetro.cpp
@@ -0,0 +1,50 @@
+/** 
+ * @file	perimetro.cpp
+ * @brief	Arquivo corpo com a implementacao das funcoes que realizam
+ *   				o calculo do perimetro de viguras geometricas planas
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	12/03/2017
+ * @date 	12/03/2017
+ * @sa		perimetro.h
+ */
+
+#include "perimetro.h"
+
+/**
+ * @brief Funcao que calcula o perimetro de um triangulo
+ * @param lado Medida do lado triangulo  
+ * @return Perimetro do triangulo
+ */
+float periTriangulo(float lado){
+	return 3 * lado;
+}
+
+
+/**
+ * @brief Funcao que calcula o perimetro do retangulo
+ * @param base Medida da base do retangulo
+ * @param altura Medida da altura do retangulo 
+ * @return Perimetro do retangulo
+ */
+float periRetangulo(float base, float altura){
+	return 2 * (base + altura);
+}
+
+/**
+ * @brief Funcao que calcula o perimetro de um quadrado
+ * @param lado Medida do lado do quadrado
+ * @return Perimetro do quadrado
+ */
+float periQuadrado(float lado){
+	return 4 * lado;
+}
+
+
+ /**
+  * @brief Funcao que calcula o perimetro do circulo
+  * @param raio Medida do raio do circulo
+  * @return Perimetro do ciculo
+  */
+float periCiculo(float raio){
+	return 2 * PI * raio;
+}
